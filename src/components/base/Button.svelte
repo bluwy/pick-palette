13c060c00d105a3cf93cc6ef71e4c5e97e3ed24f
BjornLuG <bjornlu.dev@gmail.com>
2020-06-10 21:30:52 +0800

    Update tailwind theme

--- src/components/base/Button.svelte
+++ src/components/base/Button.svelte
@@ -3,8 +3,8 @@
 </script>
 
 <button
-  class="bg-purple-500 border-2 border-purple-500 rounded-lg px-3 py-1
-  transition-colors duration-200 {outline ? 'text-purple-800 bg-opacity-0 hover:bg-opacity-25 focus:bg-opacity-25' : 'text-white hover:bg-opacity-75 focus:bg-opacity-75'}"
+  class="bg-primary-500 border-2 border-primary-500 rounded-lg px-3 py-1
+  transition-colors duration-200 {outline ? 'text-primary-800 bg-opacity-0 hover:bg-opacity-20 focus:bg-opacity-20' : 'text-white hover:bg-opacity-80 focus:bg-opacity-80'}"
   on:click
 >
   <slot />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { colors } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: ['./src/**/*.svelte'],
   theme: {
@@ -5,8 +7,51 @@ module.exports = {
       center: true,
       padding: '1rem'
     },
-    extend: {}
-  },
-  variants: {},
-  plugins: []
+    colors: {
+      transparent: colors.transparent,
+      current: colors.current,
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      primary: colors.purple,
+      info: colors.blue,
+      success: colors.green,
+      warning: colors.yellow,
+      error: colors.red
+    },
+    opacity: {
+      '0': '0',
+      '10': '.1',
+      '20': '.2',
+      '30': '.3',
+      '40': '.4',
+      '50': '.5',
+      '60': '.6',
+      '70': '.7',
+      '80': '.8',
+      '90': '.9',
+      '100': '1'
+    },
+    extend: {
+      fontFamily: {
+        sans: [
+          'Inter',
+          'system-ui',
+          '-apple-system',
+          'BlinkMacSystemFont',
+          '"Segoe UI"',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          '"Noto Sans"',
+          'sans-serif',
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+          'sans-serif'
+        ]
+      }
+    }
+  }
 }
