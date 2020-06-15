9b2010142758a847ab2742c71b1404029a7a4271
BjornLuG <bjornlu.dev@gmail.com>
2020-06-15 23:08:36 +0800

    Add editor new color view

--- src/views/Editor.svelte
+++ src/views/Editor.svelte
@@ -1,9 +1,15 @@
 <script>
   import { state } from '../store'
+  import Button from '../components/base/Button.svelte'
   import TheHeader from '../components/TheHeader.svelte'
   import ColorTab from '../components/ColorTab.svelte'
+  import EditorNewColor from './EditorNewColor.svelte'
 
   $: project = $state.projects[$state.selected]
+
+  const EDITOR_NEW_COLOR = 'editor-new-color'
+
+  let editorState = ''
 </script>
 
 <div class="h-full flex flex-col">
@@ -20,19 +26,25 @@
           {:else}
             <ul>
               {#each project.colors as color}
-                <li>
+                <li class="mb-4">
                   <ColorTab {...color} />
                 </li>
               {/each}
             </ul>
           {/if}
         </div>
+        <Button outline on:click={() => (editorState = EDITOR_NEW_COLOR)}>
+          Add color
+        </Button>
       </div>
       <div
         class="w-full h-full md:w-2/3 pt-3 md:pt-0 md:pl-6 border-t
         border-gray-500 border-opacity-50 md:border-t-0 border-l"
       >
         <div class="opacity-70 mb-5">Editor</div>
+        {#if editorState == EDITOR_NEW_COLOR}
+          <EditorNewColor />
+        {/if}
       </div>
     </div>
   </div>
---
+++ src/views/EditorNewColor.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { createEventDispatcher } from 'svelte'
+  import Button from '../components/base/Button.svelte'
+
+  const dispatch = createEventDispatcher()
+</script>
+
+<div class="text-center">
+  <h1 class="text-xl mb-10">Choose how to create your new color</h1>
+  <Button on:click={dispatch('smartcreate')}>Smart create</Button>
+  <p class="mt-2 opacity-70">
+    Give us a few colors and we will generate all the shades!
+  </p>
+  <p class="my-4 opacity-50">or</p>
+  <Button on:click={dispatch('manualcreate')}>Manual create</Button>
+  <p class="mt-2 opacity-70">
+    Start with empty shades and manually add colors.
+  </p>
+</div>
