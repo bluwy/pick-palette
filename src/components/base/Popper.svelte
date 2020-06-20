bd5ceb73a372a07ceee5bd1b06aa97c711014f0f
BjornLuG <bjornlu.dev@gmail.com>
2020-06-20 14:29:31 +0800

    Manually apply popper styles

--- src/components/base/Popper.svelte
+++ src/components/base/Popper.svelte
@@ -1,31 +1,64 @@
 <script>
   import { createPopper } from '@popperjs/core'
+  import produce from 'immer'
   import { onDestroy } from 'svelte'
 
   export let options = undefined
   export let show = false
-  export let state = undefined
 
   let reference
   let popper
   let popperInstance
+  let popperState
 
-  // Get popper state
-  $: state = popperInstance != null ? popperInstance.state : undefined
+  const css = (obj = {}) =>
+    Object.entries(obj)
+      .map((kv) => kv.join(':'))
+      .join(';')
 
-  // Set popper options whenver options changed
-  $: popperInstance != null && popperInstance.setOptions(options)
+  // Disable applyStyles modifier, we apply ourselves.
+  // Also hook update state modifier because the modifiers don't execute
+  // immediately on createPopper (for some reason)
+  $: popperOptions = produce(options || {}, (v) => {
+    v.modifiers = v.modifiers || []
+    v.modifiers.push(
+      {
+        name: 'applyStyles',
+        enabled: false
+      },
+      {
+        name: 'updateState',
+        enabled: true,
+        phase: 'write',
+        fn: ({ state }) => (popperState = state)
+      }
+    )
+  })
 
   $: show ? create() : destroy()
 
+  // Set popper options whenever options changed
+  $: popperInstance != null && popperInstance.setOptions(popperOptions)
+
+  $: popperAttrs =
+    popperState != null && popperState.attributes != null
+      ? popperState.attributes.popper
+      : {}
+
+  $: popperStyle =
+    popperState != null && popperState.styles != null
+      ? css(popperState.styles.popper)
+      : ''
+
   const create = () => {
-    popperInstance = createPopper(reference, popper, options)
+    popperInstance = createPopper(reference, popper, popperOptions)
   }
 
   const destroy = () => {
     if (popperInstance != null) {
       popperInstance.destroy()
       popperInstance = undefined
+      // Dont clear popperState for correct transitions
     }
   }
 
@@ -35,7 +68,7 @@
 <div bind:this={reference}>
   <slot name="reference" />
 </div>
-<div bind:this={popper} class="absolute">
+<div bind:this={popper} class="absolute" {...popperAttrs} style={popperStyle}>
   {#if show}
     <slot />
   {/if}
