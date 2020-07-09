1722dd5b80b5d5b025d55faaf1b5cee6c2b92494
BjornLuG <bjornlu.dev@gmail.com>
2020-07-09 22:04:36 +0800

    Add import project input

--- src/views/Dashboard.svelte
+++ src/views/Dashboard.svelte
@@ -1,10 +1,22 @@
 <script>
+  import { faFileImport } from '@fortawesome/free-solid-svg-icons'
   import { nanoid } from 'nanoid/non-secure'
+  import { coerce, StructError } from 'superstruct'
+  import Icon from 'svelte-fa'
   import { Navigate, navigateTo } from 'svelte-router-spa'
   import { openedProjects } from '@/store/project'
+  import { addSnack } from '@/store/snackbar'
   import { state } from '@/store/state'
+  import { Project } from '@/utils/validation-structs'
 
   let newProjectName = ''
+  let importFiles
+
+  $: if (importFiles && importFiles[0]) {
+    const reader = new FileReader()
+    reader.onload = (e) => importProject(JSON.parse(e.target.result))
+    reader.readAsText(importFiles[0])
+  }
 
   function createProject() {
     if (!newProjectName) {
@@ -22,15 +34,40 @@
       })
     })
 
-    // Prevent undo after navigate
+    openProject(projectId)
+  }
+
+  function importProject(data) {
+    try {
+      const projectData = coerce(data, Project)
+
+      // Make sure there's no duplicate id, if the user supplied one
+      if ($state.projects.some((v) => v.id === projectData.id)) {
+        projectData.id = nanoid(6)
+      }
+
+      state.update('Import project', (state) => {
+        state.projects.push(projectData)
+      })
+
+      openProject(projectData.id)
+    } catch (e) {
+      // Ahh error. Show snackbar.
+      // TODO: Better error display
+      addSnack({ type: 'error', message: e.message })
+    }
+  }
+
+  // TODO: Move this logic to nav event (Require router support)
+  function openProject(id) {
     state.clear()
-    openedProjects.add(projectId)
-    navigateTo(`project/${projectId}`)
+    openedProjects.add(id)
+    navigateTo(`project/${id}`)
   }
 </script>
 
 <div class="container w-full h-full flex flex-col justify-center items-center">
-  <div class="text-center">
+  <div class="text-center my-10">
     <p class="text-lg opacity-80 mb-3">Ready to pick a palette?</p>
     <form novalidate on:submit|preventDefault={createProject}>
       <input
@@ -41,10 +78,36 @@
       />
       <button type="submit">Create</button>
     </form>
+    {#if $state.projects.length <= 0}
+      <p class="my-5 text-sm opacity-60">or</p>
+      <label class="relative inline-block">
+        <button class="button--outline">
+          <span>Import a project</span>
+        </button>
+        <input
+          class="absolute top-0 left-0 w-full h-full opacity-0"
+          type="file"
+          bind:files={importFiles}
+        />
+      </label>
+    {/if}
   </div>
   {#if $state.projects.length > 0}
-    <div class="text-center mt-10">
-      <p class="opacity-80 mb-2">Or continue on previous projects:</p>
+    <div class="w-full max-w-4xl">
+      <div class="flex justify-between items-center">
+        <div class="text-xl">My projects</div>
+        <label class="relative">
+          <button>
+            <Icon icon={faFileImport} class="inline-block mr-1" />
+            <span>Import project</span>
+          </button>
+          <input
+            class="absolute top-0 left-0 w-full h-full opacity-0"
+            type="file"
+            bind:files={importFiles}
+          />
+        </label>
+      </div>
       <div>
         {#each $state.projects as project (project.id)}
           <Navigate
