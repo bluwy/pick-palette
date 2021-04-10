f88c7e40cd78b96332aa0802d8c16ac87f5a8abf
bluwy <bjornlu.dev@gmail.com>
2021-04-10 21:42:18 +0800

    Update vite-plugin-svelte

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,8 @@ import { defineConfig } from 'vite'
 import svelte from '@sveltejs/vite-plugin-svelte'
 import windicss from 'vite-plugin-windicss'
 
-const windicssPlugins = windicss()
-
-// Once @sveltejs/vite-plugin-svelte makes a new release, this is not needed
-// @ts-expect-error
-// prettier-ignore
-const windicssPreprocess = windicssPlugins.find(v => v.name.includes(':css:svelte')).sveltePreprocess
-
 export default defineConfig({
-  plugins: [
-    svelte({ useVitePreprocess: true, preprocess: windicssPreprocess }),
-    windicssPlugins
-  ],
+  plugins: [svelte({ useVitePreprocess: true }), windicss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
